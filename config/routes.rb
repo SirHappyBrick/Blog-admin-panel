a475f16bbaddb10674f6a5ee2530cf8c3c0f552c
Oskar <oskar.madej.wroc@gmail.com>
2021-10-29 04:57:20 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :authors
   get 'home/index'
 
-  root to: "home#index"
+  root to: "authors/posts#index"
   scope module: 'authors' do
     resources :posts do
       resources :elements 
